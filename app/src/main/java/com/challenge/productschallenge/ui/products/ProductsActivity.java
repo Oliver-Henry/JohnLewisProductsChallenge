eeef0dc1cc7b6bf5563d902f66166cfdbcb87b2e
Oliver <oli_henry_dev@gmail.com>
2019-03-28 12:38:46 +0000

    Rename MainActivity to ProductsActivity

---
+++ app/src/main/java/com/challenge/productschallenge/ui/products/ProductsActivity.java
@@ -0,0 +1,16 @@
+package com.challenge.productschallenge.ui.products;
+
+import android.os.Bundle;
+
+import com.challenge.productschallenge.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}

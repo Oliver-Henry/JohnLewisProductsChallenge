e32dac84e6908a9dee76ff86fe39698701f88d82
Oliver <oli_henry_dev@gmail.com>
2019-04-02 13:07:33 +0100

    Add some test cases for the ColorConverter

--- app/src/test/java/com/challenge/productschallenge/common/ColorConverterTest.java
+++ app/src/test/java/com/challenge/productschallenge/common/ColorConverterTest.java
@@ -1,7 +1,31 @@
 package com.challenge.productschallenge.common;
 
-import static org.junit.Assert.*;
+import com.challenge.productschallenge.common.base.BasicColor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ColorConverterTest {
+
+    private ColorConverter colorConverter;
+
+    @Before
+    public void setUp() {
+        colorConverter = new ColorConverter();
+    }
+
+    @Test
+    public void givenValidBasicColorReturnHex() {
+        String rgbColor = colorConverter.getRGBColorFor(BasicColor.Acqua.name());
+        assertEquals("#00FFFF", rgbColor);
+    }
+
+    @Test
+    public void givenInvalidColorReturnDefault() {
+        String rgbColor = colorConverter.getRGBColorFor("Khaki");
+        assertEquals("#FFFFFF", rgbColor);
+    }
 
 }
